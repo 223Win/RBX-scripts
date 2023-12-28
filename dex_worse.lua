e131db8219d48aa7c9d8055235215ba37dfb6aaf
223Win <112732681+223Win@users.noreply.github.com>
2023-12-27 20:56:57 -0600

    Dex worse V2

--- dex_worse.lua
+++ dex_worse.lua
@@ -1,6 +1,6 @@
 warn('DEX V2 WORSE')
 warn('LOADING DEX')
-local dex = game:GetObjects('rbxassetid://15777444980')[1]
+local dex = game:GetObjects('rbxassetid://15778218268')[1]
 warn('FINISHED')
 wait(3)
 warn('EXECUTING DEX LAG MAY BE EXPERIENCED')
