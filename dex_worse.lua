bc7e9474afb7bf85eb94dc2f57c344351939d3c1
223Win <112732681+223Win@users.noreply.github.com>
2023-12-27 20:38:09 -0600

    Dex worse v2

--- dex_worse.lua
+++ dex_worse.lua
@@ -1,10 +1,10 @@
-warn('DEX V1 WORSE')
+warn('DEX V2 WORSE')
 warn('LOADING DEX')
 local dex = game:GetObjects('rbxassetid://15777444980')[1]
 warn('FINISHED')
 wait(3)
 warn('EXECUTING DEX LAG MAY BE EXPERIENCED')
-
+wait(3)
 dex.Name = game:GetService("HttpService"):GenerateGUID(false)
 dex.Parent = game:GetService("CoreGui")
 
@@ -17,6 +17,8 @@ dex.Parent = game:GetService("CoreGui")
 for i,v in pairs(dex:GetDescendants())do if v:IsA("LuaSourceContainer") then 
 		--v.Enabled = true;
 		-- gonna use sandbox loader cause its way better and this method sucks
-		task.spawn(setfenv(loadstring(v.Source, '='..v:GetFullName()),setmetatable({script = v},{__index = getfenv()})))
+		coroutine.wrap(function()
+			task.spawn(setfenv(loadstring(v.Source, '='..v:GetFullName()),setmetatable({script = v},{__index = getfenv()})))
+		end)()
 	end;
 end;
